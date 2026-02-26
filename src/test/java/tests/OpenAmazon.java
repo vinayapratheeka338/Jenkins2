feb7439969966d325206e9d4cc24e720c7132433
VinayaPratheeka <VINAYAPRATHEEKA333@GMAIL.COM>
2026-02-26 15:34:59 +0530

    Maven Project added

---
+++ src/test/java/tests/OpenAmazon.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenAmazon {
+	@Test
+    public void searchMobile() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.in");
+        Thread.sleep(3000);
+        System.out.println(driver.getTitle());
+        driver.quit();
+	}
+}
